09a855b4290aa3c68bcced1eb13951d92e540a55
pejuangcode <hello.musliadi@gmail.com>
2022-07-06 11:18:33 +0700

    update API

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,128 @@
+<?php
+     
+namespace App\Http\Controllers\API;
+     
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Post;
+use Validator;
+use App\Http\Resources\PostResource;
+use App\Http\Requests\StorePostRequest;
+use Illuminate\Support\Facades\DB;
+
+     
+class PostController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with('user', 'comments', 'comments.replies',  'comments.user', 'comments.replies.user')
+        ->orderBy('id', 'DESC')->get();
+      
+        return $this->sendResponse(PostResource::collection($posts), 'Post retrieved successfully.');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+
+        DB::beginTransaction();   
+        
+        try {
+
+            $post = Post::create([
+                'body' => $request->body,
+                'user_id' => auth()->user()->id,
+            ]);
+            DB::commit();
+     
+            return $this->sendResponse(new PostResource($post), 'Post created successfully.');
+
+        } catch(\Exception $e) {
+            
+            DB::rollBack();
+            return $this->sendError('Post not successfully.', 'Errr'); 
+        }
+    } 
+   
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+    
+        if (is_null($post)) {
+            return $this->sendError('Post not found.');
+        }
+     
+        return $this->sendResponse(new PostResource($post), 'Post retrieved successfully.');
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StorePostRequest $request, Post $post)
+    {
+             
+        DB::beginTransaction();   
+        
+        try {
+
+            $post = Post::update([
+                'body' => $request->body,
+            ]);
+            DB::commit();
+
+            return $this->sendResponse(new PostResource($post), 'Post updated successfully.');
+
+        } catch(\Exception $e) {
+            
+            DB::rollBack();
+            return $this->sendError('Post update not successfully.', 'Errr'); 
+        }
+
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+
+        DB::beginTransaction();   
+        
+        try {
+
+            $post->delete();
+            DB::commit();
+
+            return $this->sendResponse([], 'Post deleted successfully.');
+
+        } catch(\Exception $e) {
+            
+            DB::rollBack();
+            return $this->sendError('Post update not successfully.', 'Errr'); 
+        }
+     
+
+    }
+}
---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,72 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Support\Facades\DB;
+   
+class RegisterController extends BaseController
+{
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        DB::beginTransaction();   
+        
+        try {
+
+            $input = $request->all();
+            $input['password'] = bcrypt($input['password']);
+
+            $user = User::create($input);
+            DB::commit();
+     
+            $success['token'] =  $user->createToken('MyApp')->accessToken;
+            $success['name'] =  $user->name;
+       
+            return $this->sendResponse($success, 'User register successfully.');
+
+        } catch(\Exception $e) {
+            
+            DB::rollBack();
+            return $this->sendError('User not successfully.', 'Errr'); 
+        }
+    }
+   
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            $success['name'] =  $user->name;
+   
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+}
